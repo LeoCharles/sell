340c0462d4c26b8363695a486ce0d4a4808a3f5c
leo <leo-fe@foxmail.com>
2018-01-16 14:27:13 +0800

    update

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -10,10 +10,10 @@
           <span class="name">{{seller.name}}</span>
         </div>
         <div class="description">
-          {{seller.description}}/{{seller.deliveryTime}}
+          {{seller.description}} / {{seller.deliveryTime}}
         </div>
         <div v-if="seller.supports" class="support">
-          <span class="icon"></span>
+          <span class="icon" :class="classMap[seller.supports[0].type]"></span>
           <span class="text">{{seller.supports[0].description}}</span>
         </div>
       </div>
@@ -28,6 +28,9 @@ export default {
     seller: {
       type: Object
     }
+  },
+  created () {
+    this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee']
   }
 }
 </script>
@@ -43,10 +46,12 @@ export default {
       font-size: 0
       .avatar
         display: inline-block
+        vertical-align: top
+        img 
+          border-radius: 2px
       .content
         display: inline-block
         margin-left: 16px
-        font-size: 14px
         .title
           margin: 2px 0 8px 0
           .brand
@@ -62,5 +67,30 @@ export default {
             line-height: 18px
             font-size: 16px
             font-weight: bold
-
+        .description
+          margin-bottom: 10px
+          line-height: 12px
+          font-size: 12px
+        .support
+          .icon
+            display: inline-block
+            width: 12px
+            height: 12px
+            margin-right: 4px
+            vertical-align: top
+            background-size: 12px 12px
+            background-repeat: no-repeat
+            &.decrease
+              bg-image('decrease_1')
+            &.discount
+              bg-image('discount_1')
+            &.guarantee
+              bg-image('guarantee_1')
+            &.invoice
+              bg-image('invoice_1')
+            &.special
+              bg-image('special_1')
+          .text
+            line-height: 12px
+            font-size:10px
 </style>
