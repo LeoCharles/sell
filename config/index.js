eac36bf971ccfd5a5bad47a24ad6e390d62f0ad7
leo <leo-fe@foxmail.com>
2018-01-16 18:22:07 +0800

    update

--- config/index.js
+++ config/index.js
@@ -13,7 +13,7 @@ module.exports = {
     proxyTable: {},
 
     // Various Dev Server settings
-    host: '192.168.5.169', // can be overwritten by process.env.HOST
+    host: 'localhost', // can be overwritten by process.env.HOST 192.168.5.169
     port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: true,
     errorOverlay: true,
--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -4,6 +4,7 @@
       <div class="avatar">
         <img :src="seller.avatar" width="64" height="64">
       </div>
+      <!-- 内容 -->
       <div class="content">
         <div class="title">
           <span class="brand"></span>
@@ -17,31 +18,77 @@
           <span class="text">{{seller.supports[0].description}}</span>
         </div>
       </div>
+      <div @click="showDetail" v-if="seller.supports" class="support-count">
+        <span class="count">{{seller.supports.length}}个</span>
+        <i class="icon-keyboard_arrow_right"></i>
+      </div>
     </div>
-    <div class="bulletin-wrapper"></div>
+    <!-- 公告 -->
+    <div @click="showDetail" class="bulletin-wrapper">
+      <span class="bulletin-title"></span><span class="bulletin-text">{{seller.bulletin}}</span>
+      <i class="icon-keyboard_arrow_right"></i>
+    </div>
+    <!-- 模糊背景图 -->
+    <div class="background">
+      <img :src="seller.avatar" width="100%" height="100%">
+    </div>
+    <!-- 弹出层 -->
+    <div v-show="detailShow" class="detail">
+      <div class="detail-wrapper clearfix">
+        <div class="detail-main">
+          <h1 class="name">{{seller.name}}</h1>
+          <div class="star-wrapper">
+            <!-- 评分组件 -->
+            <star :size="48" :score="seller.score"></star>
+          </div>
+          
+        </div>
+      </div>
+      <div class="detail-close">
+        <i class="icon-close"></i>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-  props: {
-    seller: {
-      type: Object
+  import star from 'components/star/star'
+
+  export default {
+    props: {
+      seller: {
+        type: Object
+      }
+    },
+    data () {
+      return {
+        detailShow: false
+      }
+    },
+    components: {
+      star
+    },
+    created () {
+      this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee']
+    },
+    methods: {
+      showDetail () {
+        this.detailShow = true
+      }
     }
-  },
-  created () {
-    this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee']
   }
-}
 </script>
 
 <style lang="stylus" scoped>
   @import "../../common/stylus/mixin.styl"
 
   .header
+    position: relative
     color: #fff
-    background-color: #666
+    background-color: rgba(7, 17, 27, .5)
+    overflow: hidden
     .content-wrapper
+      position: relative
       padding: 24px 12px 18px 24px
       font-size: 0
       .avatar
@@ -93,4 +140,87 @@ export default {
           .text
             line-height: 12px
             font-size:10px
+      .support-count
+        position: absolute 
+        right: 12px
+        bottom: 14px
+        padding: 0 8px
+        height: 24px
+        line-height: 24px
+        border-radius: 14px
+        background-color: rgba(0, 0, 0, .2)
+        text-align: center
+        .count
+          font-size: 10px
+          vertical-align: top
+        .icon-keyboard_arrow_right
+          margin-left: 2px
+          line-height: 24px
+          font-size: 10px
+    .bulletin-wrapper
+      position: relative
+      height: 28px
+      line-height: 28px
+      padding: 0 22px 0 12px
+      white-space: nowrap
+      overflow: hidden
+      text-overflow: ellipsis
+      background-color: rgba(7, 17, 27, .2)
+      .bulletin-title
+        display: inline-block
+        margin-top: 8px
+        width: 22px
+        height: 12px
+        bg-image('bulletin')
+        background-size: 22px 12px
+        background-repeat: no-repeat
+        vertical-align: top
+      .bulletin-text
+        margin: 0 4px
+        font-size: 10px
+        vertical-align: top
+      .icon-keyboard_arrow_right
+        position: absolute
+        right: 12px
+        top: 8px
+        font-size: 10px
+    .background
+      position: absolute
+      top: 0
+      left: 0
+      width: 100%
+      height: 100%
+      z-index: -1
+      filter: blur(10px)
+    .detail
+      position: fixed
+      top: 0
+      left: 0
+      width: 100%
+      height: 100%
+      z-index: 999
+      background-color: rgba(7, 17, 27, .7)
+      overflow: auto
+      .detail-wrapper
+        width: 100%
+        min-height: 100%
+        .detail-main
+          margin-top: 64px
+          padding-bottom: 64px
+          .name
+            line-height: 16px
+            text-align: center
+            font-size: 16px
+            font-weight 700
+          .star-wrapper
+            margin-top: 18px
+            padding: 2px 0
+            text-align: center
+      .detail-close
+        position: relative
+        width: 32px
+        height: 32px
+        margin: -64px auto 0 auto
+        clear: both
+        font-size: 32px
 </style>
